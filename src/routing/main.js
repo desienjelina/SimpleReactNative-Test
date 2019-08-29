5bca1339e6f36cf11c415a1a0ceb790184d2cbb9
desienjelina <desilubis724@gmail.com>
2019-08-29 18:32:07 +1200

    initial commit

---
+++ src/routing/main.js
@@ -0,0 +1,89 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+import {createStackNavigator, createMaterialTopTabNavigator} from 'react-navigation'
+import mainScreen from '../screens/main'
+import createScreen from '../screens/create'
+import detailScreen from '../screens/detail'
+import editScreen from '../screens/edit'
+import profileScreen from '../screens/profile'
+
+const HeaderStyle = () => ({
+  headerStyle: {
+    backgroundColor: '#F7CA18',
+    textAlign: 'center',
+    fontWeight: 'normal'
+  },
+  headerTintColor: '#deffffff',
+})
+
+const MainTab = createMaterialTopTabNavigator({
+  MainTab: {
+    screen: mainScreen,
+    navigationOptions: (props) => ({
+      title: "PRODUCT"
+    })
+  },
+  ProfileTab: {
+    screen: profileScreen,
+    navigationOptions: (props) => ({
+      title: "ABOUT"
+    })
+  },
+}, 
+{
+  lazy: true,
+  optimizationsEnabled: true,
+  tabBarOptions:{
+    activeTintColor: '#1f1f1f',
+    inactiveBackgroundColor:'#F00',
+    inactiveTintColor: '#afafaf',
+
+    labelStyle:{
+      // color:"#1f1f1f", 
+      fontSize:14, 
+      paddingVertical: 1
+    },
+    pressColor: '#afafaf',
+    indicatorStyle:{
+      backgroundColor: '#cc1219',
+    },
+    style: {
+      backgroundColor: '#FFFFFF',
+    },
+    tabStyle: {
+      // marginBottom:2,
+      // backgroundColor: '#FFFFFF'
+    }
+  }
+});
+
+export default Main = createStackNavigator({
+  MainScreen:{
+      screen: MainTab,
+      navigationOptions: (props) => ({
+        title: "Beranda",
+      })
+  },
+  DetailScreen:{
+      screen: detailScreen,
+      navigationOptions: (props) => ({
+        title: "Detail",
+      })
+  },
+  CreateScreen:{
+      screen: createScreen,
+      navigationOptions: (props) => ({
+        title: "Create Product",
+      })
+  },
+  EditScreen:{
+      screen: editScreen,
+      navigationOptions: (props) => ({
+        title: "Edit",
+      })
+  }
+},
+{
+  initialRouteName: "MainScreen",
+  defaultNavigationOptions: HeaderStyle,
+});
---
+++ src/screens/create.js
@@ -0,0 +1,138 @@
+import React, { Component } from 'react'
+import { Text, View, TextInput, StyleSheet, TouchableOpacity} from 'react-native'
+import Resource from '../network/Resource'
+
+const myStyle = StyleSheet.create({
+  form: {
+    borderColor: "#EFEFEF", 
+    backgroundColor: "#FEFEFE",
+    borderWidth: 1, 
+    height: 30, 
+    paddingVertical: 5, 
+    paddingHorizontal: 10, 
+    marginBottom: 10
+  }
+})
+
+export default class create extends Component {
+
+  constructor(props){
+    super(props)
+
+    this.state = {
+      brandId : "",
+      colour : "",
+      description : "",
+      district : "",
+      extendInfo : "",
+      location : "",
+      productName : "",
+      province : "",
+      subDistrict : ""
+    }
+  }
+
+  submitTask(){
+    let body = {
+      "brandId" : this.state.brandId,
+      "colour" : this.state.colour,
+      "description" : this.state.description,
+      "district" : this.state.district,
+      "extendInfo" : this.state.extendInfo,
+      "location" : this.state.location,
+      "productName" : this.state.productName,
+      "province" : this.state.province,
+      "subDistrict" : this.state.subDistrict
+    }
+
+    Resource.createTask(body)
+    .then((res) => {
+      this.resetForm();
+      alert("Submit Sukses")
+    })
+    .catch((err) => {
+      alert(JSON.stringify(err))
+    })
+  }
+
+  resetForm(){
+    this.setState({
+      brandId : "",
+      colour : "",
+      description : "",
+      district : "",
+      extendInfo : "",
+      location : "",
+      productName : "",
+      province : "",
+      subDistrict : ""
+    })
+  }
+
+  render() {
+    return (
+      <View style={{padding: 30}}>
+        <TextInput 
+          style={myStyle.form} 
+          value={this.state.brandId}
+          onChangeText={(brandId) => this.setState({brandId})}
+          placeholder="Brand"
+        />
+        <TextInput 
+          style={myStyle.form} 
+          value={this.state.colour}
+          onChangeText={(colour) => this.setState({colour})}
+          placeholder="Colour"
+        />
+        <TextInput 
+          style={myStyle.form} 
+          value={this.state.description}
+          onChangeText={(description) => this.setState({description})}
+          placeholder="Description"
+        />
+        <TextInput 
+          style={myStyle.form} 
+          value={this.state.district}
+          onChangeText={(district) => this.setState({district})}
+          placeholder="District"
+        />
+        <TextInput 
+          style={myStyle.form} 
+          value={this.state.extendInfo}
+          onChangeText={(extendInfo) => this.setState({extendInfo})}
+          placeholder="Extend Info"
+        />
+        <TextInput 
+          style={myStyle.form} 
+          value={this.state.location}
+          onChangeText={(location) => this.setState({location})}
+          placeholder="Location"
+        />
+        <TextInput 
+          style={myStyle.form} 
+          value={this.state.productName}
+          onChangeText={(productName) => this.setState({productName})}
+          placeholder="Product Name"
+        />
+        <TextInput 
+          style={myStyle.form} 
+          value={this.state.province}
+          onChangeText={(province) => this.setState({province})}
+          placeholder="Province"
+        />
+        <TextInput 
+          style={myStyle.form} 
+          value={this.state.subDistrict}
+          onChangeText={(subDistrict) => this.setState({subDistrict})}
+          placeholder="Sub District"
+        />
+        
+        <TouchableOpacity style={{marginTop: 20}} onPress={() => this.submitTask()}>
+          <View style={{backgroundColor:"#F7CA18", padding: 10}}>
+            <Text style={{color:"#FFF", textAlign:"center"}}>SUBMIT</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    )
+  }
+}
